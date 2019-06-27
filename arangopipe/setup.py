6972d67b75a552e1681432728b20a34d6d3921ed
rajivsam <rajiv.sambasivan@gmail.com>
2019-06-27 22:25:00 +0530

    Readme update aql examples with changes from review

--- arangopipe/setup.py
+++ arangopipe/setup.py
@@ -2,7 +2,8 @@ import pathlib
 from setuptools import setup
 
 # The directory containing this file
-HERE = pathlib.Path(__file__).parent
+HERE = pathlib.Path(__file__).resolve().parents[1]
+
 
 # The text of the README file
 README = (HERE / "README.md").read_text()
