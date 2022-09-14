43b866b707ef0b85250d8467c23182004ac36f55
Felix Riemen <92442930+ferie24@users.noreply.github.com>
2022-09-14 23:15:39 +0200

    Release automation added (#179)
    * release files added
    
    * release files updated

--- arangopipe/setup.py
+++ arangopipe/setup.py
@@ -4,14 +4,20 @@ from setuptools import setup
 # The directory containing this file
 HERE = pathlib.Path(__file__).resolve().parents[1]
 
-
 # The text of the README file
 README = (HERE / "README.md").read_text()
+
+requirements = ['python-arango',
+                'pandas',
+                'PyYAML==5.1.1',
+                'sklearn2',
+                'yapf',
+                'autopep8']
 
 # This call to setup() does all the work
 setup(
     name="arangopipe",
-    version="0.0.70.0.0",
+    version="0.0.70.0.2",
     description="package for machine learning meta-data management and analysis",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -23,7 +29,9 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7"],
-    packages=["arangopipe", "arangopipe.arangopipe_storage","arangopipe.arangopipe_analytics"],
+    packages=["arangopipe", "arangopipe.arangopipe_storage", "arangopipe.arangopipe_analytics"],
     package_data={'config': ['arangopipe/arangopipe_storage/arangopipe_config.yaml']},
-    include_package_data=True
+    include_package_data=True,
+    install_requires=requirements,
+    python_requries=">=3.7"
 )
